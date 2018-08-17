4c36f929007930914c28135e4d0eb936b6bb938e
Zheng Kai <zhengkai@gmail.com>
2018-08-17 17:05:13 +0800

    up

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ sudo chown -R zhengkai:zhengkai ~/
 
 git clone https://github.com/zhengkai/config.git ~/conf
 
+~/conf/script/update-ubuntu.sh
+
 ~/conf/apt/aptget.sh
 
 ~/conf/link.sh
@@ -20,3 +22,8 @@ git submodule update --init --recursive
 vim +PlugInstall +qall
 
 sudo chsh -s /bin/zsh zhengkai
+
+git clone https://github.com/zhengkai/build.git ~/build
+~/build/rc-local/install.sh
+
+sudo cp ~/build/shadowsocks/20-shadowsocks.conf /etc/sysctl.d/
